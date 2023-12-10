c4a0578ab0ef991bc249c70afe20920f2dfa9baa
dnhuang <dnhuang@berkeley.edu>
2023-12-09 17:54:19 -0800

    Create locate.py, pushing to migrate to windows development

---
+++ e7-shop-refresher/locate.py
@@ -0,0 +1,13 @@
+import pyautogui
+
+#res = pyautogui.locateOnScreen("images/test_img.png")
+#print(res)
+#test_button = pyautogui.center(res)
+#pyautogui.moveTo(test_button)
+
+#refresh_location = pyautogui.locateOnScreen("images/refresh.png")
+#print(refresh_location)
+
+
+test_location = pyautogui.locateOnScreen("images/test2.png")
+print(test_location)

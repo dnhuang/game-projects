480c859a717e86ebb36c0800294693594810d6af
dnhuang <dnhuang@berkeley.edu>
2023-12-09 21:50:45 -0800

    Remove test.py

--- e7-shop-refresher/test.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-
-coord = pyautogui.locateCenterOnScreen("images/edit_button.png",
-                                       grayscale=True, confidence=0.5)
-offset_coord = coord.x + 10, coord.y+10
-print(offset_coord)
-print(type(offset_coord))
-print(offset_coord[1])
